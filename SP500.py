046c9feccaa81e3217d4bd4965ad8f9027852d08
Andrew <andrewhuh67@gmail.com>
2018-07-22 21:26:17 -0700

    added the data grab from yahoo finance

--- SP500.py
+++ SP500.py
@@ -29,3 +29,37 @@ def save_sp500_tickers():
 
 	print(tickers)
 	return tickers
+
+# gets ticker data for each company from yahoo and saves it to csv
+# 2 companies did not pass
+def get_data_from_yahoo(reload_sp500=False):
+	if reload_sp500:
+		tickers = save_sp500_tickers()
+	else:
+		with open("sp500tickers.pickle", "rb") as f:
+			tickers = pickle.load(f)
+
+	if not os.path.exists('stock_dfs'):
+		os.makedirs('stock_dfs')
+
+	start = dt.datetime(2010, 1, 1)
+	end = dt.datetime.now()
+
+	for ticker in tickers:
+		print(ticker)
+		if not os.path.exists('stock_dfs/{}.cvs'.format(ticker)):
+			try:
+				yf.pdr_override()
+				# data = yf.download(ticker, start, end)
+				df = web.get_data_yahoo(ticker, start, end)
+				# df.reset_index(inplace=True)
+				# df.set_index("Date", inplace=True)
+				# df = df.drop("Symbol", axis=1)
+				df.to_csv('stock_dfs/{}.csv'.format(ticker))
+				time.sleep(0.5)
+			except:
+				pass
+
+
+		else:
+			print('Already have {}'.format(ticker))
